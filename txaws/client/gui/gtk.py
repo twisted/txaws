4bbbe50d29f44603c4a95bb852355e08e6bfb9b7
Robert Collins <robertc@robertcollins.net>
2009-04-30 23:49:08 +1000

    Support prompting for user credentials.

--- txaws/client/gui/gtk.py
+++ txaws/client/gui/gtk.py
@@ -7,7 +7,11 @@ from __future__ import absolute_import
 
 __all__ = ['main']
 
+import gnomekeyring
+import gobject
 import gtk
+
+from txaws.credentials import AWSCredentials
 
 class AWSStatusIcon(gtk.StatusIcon):
     """A status icon shown when instances are running."""
@@ -20,9 +24,12 @@ class AWSStatusIcon(gtk.StatusIcon):
         self.connect('activate', self.on_activate)
         self.probing = False
         # Nested import because otherwise we get 'reactor already installed'.
-        from txaws.ec2.client import EC2Client
-        self.client = EC2Client()
-        self.on_activate(None)
+        self.password_dialog = None
+        try:
+            creds = AWSCredentials()
+        except ValueError:
+            creds = self.from_gnomekeyring()
+        self.create_client(creds)
         menu = '''
             <ui>
              <menubar name="Menubar">
@@ -45,9 +52,60 @@ class AWSStatusIcon(gtk.StatusIcon):
         self.menu = self.manager.get_widget('/Menubar/Menu/Stop instances').props.parent
         self.connect('popup-menu', self.on_popup_menu)
 
+    def create_client(self, creds):
+        from txaws.ec2.client import EC2Client
+        if creds is not None:
+            self.client = EC2Client(creds=creds)
+            self.on_activate(None)
+        else:
+            # waiting on user entered credentials.
+            self.client = None
+
+    def from_gnomekeyring(self):
+        # Try for gtk gui specific credentials.
+        creds = None
+        try:
+            items = gnomekeyring.find_items_sync(
+                gnomekeyring.ITEM_GENERIC_SECRET,
+                {
+                    'aws-host': 'aws.amazon.com',
+                })
+        except (gnomekeyring.NoMatchError,
+            gnomekeyring.DeniedError):
+            self.password_dialog = gtk.Dialog("Enter your AWS credentals", None, gtk.DIALOG_MODAL,
+                (gtk.STOCK_OK, gtk.RESPONSE_ACCEPT,
+                gtk.STOCK_CANCEL,
+                gtk.RESPONSE_REJECT))
+            content = self.password_dialog.get_content_area()
+            def add_entry(name):
+                box = gtk.HBox()
+                box.show()
+                content.add(box)
+                label = gtk.Label(name)
+                label.show()
+                box.add(label)
+                entry = gtk.Entry()
+                entry.show()
+                box.add(entry)
+                label.set_use_underline(True)
+                label.set_mnemonic_widget(entry)
+            add_entry("AWS _Access Key ID")
+            add_entry("AWS _Secret Key")
+
+            self.password_dialog.show()
+            self.password_dialog.connect('response', self.save_key)
+            self.password_dialog.run()
+
+            # couldn't get the item.  Maybe prompt user and insert new item?
+            return None
+        else:
+            key_id, secret_key = items[0].secret.split(':')
+            return AWSCredentials(access_key=key_id, secret_key=secret_key)
+
     def on_activate(self, data):
-        if self.probing:
-            # don't ask multiple times.
+        if self.probing or not self.client:
+            # don't ask multiple times, and don't ask until we have
+            # credentials.
             return
         self.probing = True
         self.client.describe_instances().addCallbacks(self.showhide, self.errorit)
@@ -59,6 +117,29 @@ class AWSStatusIcon(gtk.StatusIcon):
         # It would be nice to popup a window to select instances.. TODO.
         self.client.describe_instances().addCallbacks(self.shutdown_instances,
             self.errorit)
+
+    def save_key(self, response_id, data):
+        # handle the dialog
+        dialog = self.password_dialog
+        try:
+            if data != gtk.RESPONSE_ACCEPT:
+                # User cancelled. They can ask for the password again somehow.
+                return
+            content = self.password_dialog.get_content_area()
+            key_id = content.get_children()[0].get_children()[1].get_text()
+            secret_key = content.get_children()[1].get_children()[1].get_text()
+            creds = AWSCredentials(access_key=key_id, secret_key=secret_key)
+            self.create_client(creds)
+            gnomekeyring.item_create_sync(
+                None,
+                gnomekeyring.ITEM_GENERIC_SECRET,
+                'AWS access credentials',
+                    {'aws-host': 'aws.amazon.com'},
+                    "%s:%s" % (key_id, secret_key), True)
+        finally:
+            self.password_dialog.hide()
+            # XXX? Does this leak?
+            self.password_dialog = None
 
     def showhide(self, reservation):
         active = 0
@@ -100,4 +181,5 @@ def main(argv, reactor=None):
         gtk2reactor.install()
         from twisted.internet import reactor
     status = AWSStatusIcon(reactor)
+    gobject.set_application_name('aws-status')
     reactor.run()
