c740064619207135eeaf3cdaac8f9f82432cec83
Duncan McGreggor <oubiwann@ubuntu.com>
2009-08-13 15:17:00 -0300

    Changed the method names from Twisted CamelCase to PEP-8.

--- txaws/ec2/exception.py
+++ txaws/ec2/exception.py
@@ -19,48 +19,55 @@ class EC2Error(AWSError):
         self.parse()
 
     def __str__(self):
-        return self._getErrorMessageString()
+        return self._get_error_message_string()
 
     def __repr__(self):
         return "<%s object with %s>" % (
-            self.__class__.__name__, self._getErrorCodeString())
+            self.__class__.__name__, self._get_error_code_string())
 
-    def _getErrorCodeString(self):
+    def _get_error_code_string(self):
         count = len(self.errors)
         if count > 1:
             codeString = "Error count: %s" % count
         else:
-            # what if no errors? what if no code?
+            # XXX what if no errors? what if no code?
             codeString = "Error code: %s" % self.errors[0]["Code"]
         return codeString
 
-    def _getErrorMessageString(self):
+    def _get_error_message_string(self):
         count = len(self.errors)
         if count > 1:
             messageString = "Multiple EC2 Errors."
         else:
+            # XXX what if no errors? what if no message?
             messageString = "Error Message: %s" % self.errors[0]["Message"]
         return messageString
 
-    def _nodeToDict(self, node):
+    def get_error_message(self):
+        pass
+
+    def get_error_messages(self):
+        pass
+
+    def _node_to_dict(self, node):
         data = {}
         for child in node:
             if child.tag and child.text:
                 data[child.tag] = child.text
         return data
 
-    def setRequestID(self, tree):
+    def set_request_id(self, tree):
         requestIDNode = tree.find(".//RequestID")
         if hasattr(requestIDNode, "text"):
             text = requestIDNode.text
             if text:
                 self.requestID = text
 
-    def setErrors(self, tree):
+    def set_errors(self, tree):
         errorsNode = tree.find(".//Errors")
         if errorsNode:
             for error in errorsNode:
-                data = self._nodeToDict(error)
+                data = self._node_to_dict(error)
                 if data:
                     self.errors.append(data)
 
@@ -68,10 +75,10 @@ class EC2Error(AWSError):
         if not xml:
             xml = self.original
         tree = ElementTree.fromstring(xml.strip())
-        self.setRequestID(tree)
-        self.setErrors(tree)
+        self.set_request_id(tree)
+        self.set_errors(tree)
 
-    def hasError(self, errorString):
+    def has_error(self, errorString):
         for error in self.errors:
             if errorString in error.values():
                 return True
