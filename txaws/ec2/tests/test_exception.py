2d81b2f4f2ecb3d6ae3094b27062b901ea2ce53f
Duncan McGreggor <duncan@canonical.com>
2009-08-13 15:17:00 -0300

    Changed the method names from Twisted CamelCase to PEP-8.

--- txaws/ec2/tests/test_exception.py
+++ txaws/ec2/tests/test_exception.py
@@ -47,116 +47,116 @@ ERROR_EXAMPLE_MULTIPLE = """
 
 class EC2ErrorTestCase(TestCase):
 
-    def test_nodeToDict(self):
+    def test_node_to_dict(self):
         xml = "<parent><child1>text1</child1><child2>text2</child2></parent>"
         error = EC2Error("<dummy />")
-        data = error._nodeToDict(ElementTree.fromstring(xml))
+        data = error._node_to_dict(ElementTree.fromstring(xml))
         self.assertEquals(data, {"child1": "text1", "child2": "text2"})
 
-    def test_setRequestID(self):
+    def test_set_request_id(self):
         xml = "<a><b /><RequestID>%s</RequestID></a>" % REQUEST_ID
         error = EC2Error("<dummy />")
-        error.setRequestID(ElementTree.fromstring(xml))
+        error.set_request_id(ElementTree.fromstring(xml))
         self.assertEquals(error.requestID, REQUEST_ID)
 
-    def test_setErrors(self):
+    def test_set_errors(self):
         errorsXML = "<Error><Code>1</Code><Message>2</Message></Error>"
         xml = "<a><Errors>%s</Errors><b /></a>" % errorsXML
         error = EC2Error("<dummy />")
-        error.setErrors(ElementTree.fromstring(xml))
+        error.set_errors(ElementTree.fromstring(xml))
         self.assertEquals(error.errors[0]["Code"], "1")
         self.assertEquals(error.errors[0]["Message"], "2")
 
-    def test_setEmptyErrors(self):
+    def test_set_empty_errors(self):
         xml = "<a><Errors /><b /></a>"
         error = EC2Error("<dummy />")
-        error.setErrors(ElementTree.fromstring(xml))
+        error.set_errors(ElementTree.fromstring(xml))
         self.assertEquals(error.errors, [])
 
-    def test_setEmptyError(self):
+    def test_set_empty_error(self):
         xml = "<a><Errors><Error /><Error /></Errors><b /></a>"
         error = EC2Error("<dummy />")
-        error.setErrors(ElementTree.fromstring(xml))
+        error.set_errors(ElementTree.fromstring(xml))
         self.assertEquals(error.errors, [])
 
-    def test_parseWithoutXML(self):
+    def test_parse_without_xml(self):
         xml = "<dummy />"
         error = EC2Error(xml)
         error.parse()
         self.assertEquals(error.original, xml)
 
-    def test_parseWithXML(self):
+    def test_parse_with_xml(self):
         xml1 = "<dummy1 />"
         xml2 = "<dummy2 />"
         error = EC2Error(xml2)
         error.parse(xml2)
         self.assertEquals(error.original, xml2)
 
-    def test_hasError(self):
+    def test_has_error(self):
         errorsXML = "<Error><Code>Code1</Code><Message>2</Message></Error>"
         xml = "<a><Errors>%s</Errors><b /></a>" % errorsXML
         error = EC2Error(xml)
-        self.assertTrue(error.hasError("Code1"))
+        self.assertTrue(error.has_error("Code1"))
 
-    def test_singleError(self):
+    def test_single_error(self):
         error = EC2Error(ERROR_EXAMPLE_SINGLE)
         self.assertEquals(len(error.errors), 1)
 
-    def test_multipleErrors(self):
+    def test_multiple_errors(self):
         error = EC2Error(ERROR_EXAMPLE_MULTIPLE)
         self.assertEquals(len(error.errors), 2)
 
-    def test_emptyXML(self):
+    def test_empty_xml(self):
         self.assertRaises(ValueError, EC2Error, "")
 
-    def test_noRequestID(self):
+    def test_no_request_id(self):
         errors = "<Errors><Error><Code /><Message /></Error></Errors>"
         xml = "<Response>%s<RequestID /></Response>" % errors
         error = EC2Error(xml)
         self.assertEquals(error.requestID, "")
 
-    def test_noRequestIDNode(self):
+    def test_no_request_id_node(self):
         errors = "<Errors><Error><Code /><Message /></Error></Errors>"
         xml = "<Response>%s</Response>" % errors
         error = EC2Error(xml)
         self.assertEquals(error.requestID, "")
 
-    def test_noErrorsNode(self):
+    def test_no_errors_node(self):
         xml = "<Response><RequestID /></Response>"
         error = EC2Error(xml)
         self.assertEquals(error.errors, [])
 
-    def test_noErrorNode(self):
+    def test_no_error_node(self):
         xml = "<Response><Errors /><RequestID /></Response>"
         error = EC2Error(xml)
         self.assertEquals(error.errors, [])
 
-    def test_noErrorCodeNode(self):
+    def test_no_error_code_node(self):
         errors = "<Errors><Error><Message /></Error></Errors>"
         xml = "<Response>%s<RequestID /></Response>" % errors
         error = EC2Error(xml)
         self.assertEquals(error.errors, [])
 
-    def test_noErrorMessageNode(self):
+    def test_no_error_message_node(self):
         errors = "<Errors><Error><Code /></Error></Errors>"
         xml = "<Response>%s<RequestID /></Response>" % errors
         error = EC2Error(xml)
         self.assertEquals(error.errors, [])
 
-    def test_singleErrorStr(self):
+    def test_single_error_str(self):
         error = EC2Error(ERROR_EXAMPLE_SINGLE)
         self.assertEquals(str(error), "Error Message: Message for Error.Code")
 
-    def test_singleErrorRepr(self):
+    def test_single_error_repr(self):
         error = EC2Error(ERROR_EXAMPLE_SINGLE)
         self.assertEquals(
             repr(error),
             "<EC2Error object with Error code: Error.Code>")
 
-    def test_multipleErrorsStr(self):
+    def test_multiple_errors_str(self):
         error = EC2Error(ERROR_EXAMPLE_MULTIPLE)
         self.assertEquals(str(error), "Multiple EC2 Errors.")
 
-    def test_multipleErrorsRepr(self):
+    def test_multiple_errors_repr(self):
         error = EC2Error(ERROR_EXAMPLE_MULTIPLE)
         self.assertEquals(repr(error), "<EC2Error object with Error count: 2>")
