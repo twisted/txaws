e6c4137c7ebeafc81dcf98baa11d370f05415d06
Duncan McGreggor <duncan@canonical.com>
2009-08-17 11:11:18 -0300

    Moved payloads used in EC2 unit tests into their own testing module.

--- txaws/ec2/tests/test_client.py
+++ txaws/ec2/tests/test_client.py
@@ -9,78 +9,8 @@ from txaws.ec2 import client
 from txaws.ec2.exception import EC2Error
 from txaws.credentials import AWSCredentials
 from txaws.tests import TXAWSTestCase
-
-
-sample_describe_instances_result = """<?xml version="1.0"?>
-<DescribeInstancesResponse xmlns="http://ec2.amazonaws.com/doc/2008-12-01/">
-    <requestId>52b4c730-f29f-498d-94c1-91efb75994cc</requestId>
-    <reservationSet>
-        <item>
-            <reservationId>r-cf24b1a6</reservationId>
-            <ownerId>123456789012</ownerId>
-            <groupSet>
-                <item>
-                    <groupId>default</groupId>
-                </item>
-            </groupSet>
-            <instancesSet>
-                <item>
-                    <instanceId>i-abcdef01</instanceId>
-                    <imageId>ami-12345678</imageId>
-                    <instanceState>
-                        <code>16</code>
-                        <name>running</name>
-                    </instanceState>
-                    <privateDnsName>domU-12-31-39-03-15-11.compute-1.internal</privateDnsName>
-                    <dnsName>ec2-75-101-245-65.compute-1.amazonaws.com</dnsName>
-                    <reason/>
-                    <keyName>keyname</keyName>
-                    <amiLaunchIndex>0</amiLaunchIndex>
-                    <productCodes/>
-                    <instanceType>c1.xlarge</instanceType>
-                    <launchTime>2009-04-27T02:23:18.000Z</launchTime>
-                    <placement>
-                        <availabilityZone>us-east-1c</availabilityZone>
-                    </placement>
-                    <kernelId>aki-b51cf9dc</kernelId>
-                    <ramdiskId>ari-b31cf9da</ramdiskId>
-                </item>
-            </instancesSet>
-        </item>
-    </reservationSet>
-</DescribeInstancesResponse>
-"""
-
-
-sample_terminate_instances_result = """<?xml version="1.0"?>
-<TerminateInstancesResponse xmlns="http://ec2.amazonaws.com/doc/2008-12-01/">
-  <instancesSet>
-    <item>
-      <instanceId>i-1234</instanceId>
-      <shutdownState>
-        <code>32</code>
-        <name>shutting-down</name>
-      </shutdownState>
-      <previousState>
-        <code>16</code>
-        <name>running</name>
-      </previousState>
-    </item>
-    <item>
-      <instanceId>i-5678</instanceId>
-      <shutdownState>
-        <code>32</code>
-        <name>shutting-down</name>
-      </shutdownState>
-      <previousState>
-        <code>32</code>
-        <name>shutting-down</name>
-      </previousState>
-    </item>
-  </instancesSet>
-</TerminateInstancesResponse>
-"""
-
+from txaws.ec2.tests.payload import (
+    sample_describe_instances_result, sample_terminate_instances_result)
 
 class FakeHTTPPageGetter(HTTPPageGetter):
 
