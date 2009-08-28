177814272f41c347c0b5e22635427ae6027b863e
Duncan McGreggor <oubiwann@ubuntu.com>
2009-08-28 16:19:33 -0300

    - Removed unimplemented methods (jkakar 1). - Made environment mutation methods private (jkakar 3). - Tweaked the default values for the FakeEC2Client (jkakar 4). - Removed unnecessary test case methods (jkakar 5).

--- txaws/testing/base.py
+++ txaws/testing/base.py
@@ -8,17 +8,17 @@ class TXAWSTestCase(TestCase):
 
     def setUp(self):
         TestCase.setUp(self)
-        self.stash_environ()
+        self._stash_environ()
 
-    def stash_environ(self):
+    def _stash_environ(self):
         self.orig_environ = dict(os.environ)
-        self.addCleanup(self.restore_environ)
+        self.addCleanup(self._restore_environ)
         if 'AWS_ACCESS_KEY_ID' in os.environ:
             del os.environ['AWS_ACCESS_KEY_ID']
         if 'AWS_SECRET_ACCESS_KEY' in os.environ:
             del os.environ['AWS_SECRET_ACCESS_KEY']
 
-    def restore_environ(self):
+    def _restore_environ(self):
         for key in set(os.environ) - set(self.orig_environ):
             del os.environ[key]
         os.environ.update(self.orig_environ)
--- txaws/testing/ec2.py
+++ txaws/testing/ec2.py
@@ -3,10 +3,10 @@ from twisted.internet.defer import succeed
 
 class FakeEC2Client(object):
 
-    def __init__(self, creds, endpoint, instances=[]):
+    def __init__(self, creds, endpoint, instances=None):
         self.creds = creds
         self.endpoint = endpoint
-        self.instances = instances
+        self.instances = instances or []
 
     def describe_instances(self):
         return succeed(self.instances)
