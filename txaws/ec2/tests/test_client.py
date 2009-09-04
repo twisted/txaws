1e50fdf9f26cb1cfcf04124b55fadbc93668a0c1
Duncan McGreggor <oubiwann@ubuntu.com>
2009-09-04 15:46:18 -0500

    Moved fake testing objects into testing module.

--- txaws/ec2/tests/test_client.py
+++ txaws/ec2/tests/test_client.py
@@ -26,81 +26,9 @@ from txaws.ec2.tests.payload import (
 from txaws.service import AWSServiceEndpoint, EC2_ENDPOINT_US
 from txaws.testing import payload
 from txaws.testing.base import TXAWSTestCase
-
-
-class FakeHTTPHandler(Request):
-    status = 200
-
-    def process(self):
-        self.content.seek(0, 0)
-        data = self.content.read()
-        length = self.getHeader('Content-Length')
-        request = "'''\n"+str(length)+"\n"+data+"'''\n"
-        self.setResponseCode(self.status)
-        self.setHeader("Request", self.uri)
-        self.setHeader("Command", self.method)
-        self.setHeader("Version", self.clientproto)
-        self.setHeader("Content-Length", len(request))
-        self.write(request)
-        self.finish()
-
-
-class FourOhHTTPHandler(FakeHTTPHandler):
-    status = 400
-
-
-class FiveOhHTTPHandler(FakeHTTPHandler):
-    status = 500
-
-
-class FakeHTTPPageGetter(HTTPPageGetter):
-
-    transport = StringTransport
-
-
-class FakeHTTPFactory(HTTPClientFactory):
-#class FakeHTTPFactory(HTTPClient):
-
-    #protocol = FakeHTTPPageGetter
-    #test_payload = ""
-    def __init__(self, test_payload, method=None):
-        self.test_payload = test_payload
-        self.method = method
-        self.wasStarted = None
-        self.wasStopped = None
-        self.connectionFailed = None
-        self.deferred = Deferred()
-
-    def connectionMade(self):
-        content_length = len(self.test_payload)
-        self.sendCommand("GET", "/dummy")
-        self.sendHeader("Content-Length", content_length)
-        self.endHeaders()
-        self.transport.write(self.test_payload)
-        self.deferred.success("success")
-"""
-    def doStart(self):
-        self.wasStarted = True
-
-    def doStop(self):
-        self.wasStopped = True
-
-    def startedConnecting(self, *args):
-        pass
-
-    def clientConnectionFailed(self, *args):
-        self.connectionFailed = True
-        self.deferred.fail("failed")
-"""
-
-class FactoryWrapper(object):
-
-    def __init__(self, payload):
-        self.payload = payload
-
-    def __call__(self, url, *args, **kwds):
-        FakeHTTPFactory.test_payload = self.payload
-        return FakeHTTPFactory(url, *args, **kwds)
+from txaws.testing.ec2 import (
+    FakeHTTPHandler, FourOhHTTPHandler, FiveOhHTTPHandler, FakeHTTPPageGetter,
+    FakeHTTPFactory, FactoryWrapper)
 
 
 class ReservationTestCase(TXAWSTestCase):
