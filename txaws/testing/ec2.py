103554e5416a38dfbe5e7dde5c27eedeafc088de
Duncan McGreggor <oubiwann@ubuntu.com>
2009-09-04 12:24:03 -0500

    Updated fake client with a means of using describe_keypairs in tests.

--- txaws/testing/ec2.py
+++ txaws/testing/ec2.py
@@ -3,10 +3,14 @@ from twisted.internet.defer import succeed
 
 class FakeEC2Client(object):
 
-    def __init__(self, creds, endpoint, instances=None):
+    def __init__(self, creds, endpoint, instances=None, keypairs=[]):
         self.creds = creds
         self.endpoint = endpoint
         self.instances = instances or []
+        self.keypairs = keypairs
 
     def describe_instances(self):
         return succeed(self.instances)
+
+    def describe_keypairs(self):
+        return succeed(self.keypairs)
