061867b9b5abca89af0edd9fdc88d0298526a80b
Duncan McGreggor <oubiwann@ubuntu.com>
2009-10-01 10:14:25 -0600

    Trivial merge [r=jkakar]
    This merge provides several improvements to the testing infrastructure.

--- txaws/testing/ec2.py
+++ txaws/testing/ec2.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2009 Duncan McGreggor <oubiwann@adytum.us>
 # Licenced under the txaws licence available at /LICENSE in the txaws source.
 
+from datetime import datetime
+
 from twisted.internet.defer import succeed, fail
 from twisted.python.failure import Failure
 from twisted.web.error import Error
@@ -17,12 +19,14 @@ class FakeEC2Client(object):
         self.endpoint = endpoint
         self.instances = instances or []
         self.keypairs = keypairs or []
-        self.volumes = volumes or []
         self.keypairs_deleted = []
+        self.volumes = volumes or []
+        self.volumes_deleted = []
         self.key_material = key_material
         self.security_groups = security_groups or []
         self.security_groups_deleted = []
         self.snapshots = snapshots or []
+        self.snapshots_deleted = []
 
     def describe_instances(self):
         return succeed(self.instances)
@@ -38,24 +42,6 @@ class FakeEC2Client(object):
         self.keypairs_deleted.append(name)
         return succeed(True)
 
-    def describe_volumes(self, *volume_ids):
-        return succeed(self.volumes)
-
-    def describe_snapshots(self, *snapshot_ids):
-        return succeed(self.snapshots)
-
-    def delete_volume(self, volume_id):
-        return succeed(True)
-
-    def delete_snapshot(self, volume_id):
-        return succeed(True)
-
-    def create_volume(self, availability_zone, size=None, snapshot_id=None):
-        return succeed(self.volumes[0])
-
-    def create_snapshot(self, volume_id):
-        return succeed(self.snapshots[0])
-
     def describe_security_groups(self, names=None):
         return succeed(self.security_groups)
 
@@ -65,6 +51,30 @@ class FakeEC2Client(object):
 
     def delete_security_group(self, name):
         self.security_groups_deleted.append(name)
+        return succeed(True)
+
+    def describe_volumes(self, *volume_ids):
+        return succeed(self.volumes)
+
+    def create_volume(self, availability_zone, size=None, snapshot_id=None):
+        return succeed(self.volumes[0])
+
+    def attach_volume(self, volume_id, instance_id, device):
+        return succeed({"status": u"attaching",
+                        "attach_time": datetime(2007, 6, 6, 11, 10, 00)})
+
+    def delete_volume(self, volume_id):
+        self.volumes_deleted.append(volume_id)
+        return succeed(True)
+
+    def describe_snapshots(self, *snapshot_ids):
+        return succeed(self.snapshots)
+
+    def create_snapshot(self, volume_id):
+        return succeed(self.snapshots[0])
+
+    def delete_snapshot(self, volume_id):
+        self.snapshots_deleted.append(volume_id)
         return succeed(True)
 
 
