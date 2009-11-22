7a8802c626ebe981ad2bc1ca9043a4130f7bb303
Duncan McGreggor <oubiwann@ubuntu.com>
2009-11-21 22:09:45 -0700

    * Moved the shared logic out of EC2Error and into AWSError. * Removed redundant logic from S3Error. * Moved shared unit tests for exceptions into top-level exception test module.

--- txaws/s3/tests/test_exception.py
+++ txaws/s3/tests/test_exception.py
@@ -14,71 +14,12 @@ REQUEST_ID = "0ef9fc37-6230-4d81-b2e6-1b36277d4247"
 
 class S3ErrorTestCase(TestCase):
 
-    def test_set_400_errors(self):
+    def test_set_400_error(self):
         xml = "<Error><Code>1</Code><Message>2</Message></Error>"
         error = S3Error("<dummy />")
-        error._set_400_errors(XML(xml))
+        error._set_400_error(XML(xml))
         self.assertEquals(error.errors[0]["Code"], "1")
         self.assertEquals(error.errors[0]["Message"], "2")
-
-    def test_set_500_error(self):
-        xml = "<Error><Code>500</Code><Message>Oops</Message></Error>"
-        error = S3Error("<dummy />")
-        error._set_500_error(XML(xml))
-        self.assertEquals(error.errors[0]["Code"], "500")
-        self.assertEquals(error.errors[0]["Message"], "Oops")
-
-    def test_set_empty_errors(self):
-        xml = "<a><Errors /><b /></a>"
-        error = S3Error("<dummy />")
-        error._set_400_errors(XML(xml))
-        self.assertEquals(error.errors, [])
-
-    def test_set_empty_error(self):
-        xml = "<a><Errors><Error /><Error /></Errors><b /></a>"
-        error = S3Error("<dummy />")
-        error._set_400_errors(XML(xml))
-        self.assertEquals(error.errors, [])
-
-    def test_parse_without_xml(self):
-        xml = "<dummy />"
-        error = S3Error(xml)
-        error.parse()
-        self.assertEquals(error.original, xml)
-
-    def test_parse_with_xml(self):
-        xml1 = "<dummy1 />"
-        xml2 = "<dummy2 />"
-        error = S3Error(xml1)
-        error.parse(xml2)
-        self.assertEquals(error.original, xml2)
-
-    def test_parse_html(self):
-        xml = "<html><body>a page</body></html>"
-        self.assertRaises(AWSResponseParseError, S3Error, xml)
-
-    def test_has_error(self):
-        xml = "<Error><Code>Code1</Code><Message>2</Message></Error>"
-        error = S3Error(xml)
-        self.assertTrue(error.has_error("Code1"))
-
-    def test_empty_xml(self):
-        self.assertRaises(ValueError, S3Error, "")
-
-    def test_empty_error_node(self):
-        xml = "<Error />"
-        error = S3Error(xml)
-        self.assertEquals(error.errors, [])
-
-    def test_no_error_message_node(self):
-        xml = "<Error><Code /></Error>"
-        error = S3Error(xml)
-        self.assertEquals(error.errors, [])
-
-    def test_zero_get_error_messages(self):
-        xml = "<Response><RequestID /></Response>"
-        error = S3Error(xml)
-        self.assertEquals(error.get_error_messages(), "Empty error list")
 
     def test_get_error_code(self):
         error = S3Error(payload.sample_s3_invalid_access_key_result)
