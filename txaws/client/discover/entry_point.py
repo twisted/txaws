3875ba86d0d1ac2721a87c2fa1cd76b3ae852b03
Jamu Kakar <jkakar@kakar.ca>
2010-06-04 21:11:40 +0200

    - Renamed txaws.client.discover.discover module to entry_point. - Added a new txaws.client.discover.command module and moved the   Command object there.

--- txaws/client/discover/entry_point.py
+++ txaws/client/discover/entry_point.py
@@ -4,7 +4,8 @@
 """A command-line client for discovering how the EC2 API works."""
 
 import os
-import sys
+
+from txaws.client.discover.command import Command
 
 
 class OptionError(Exception):
@@ -89,26 +90,6 @@ def get_command(arguments):
     endpoint = options.pop("endpoint")
     action = options.pop("action")
     return Command(key, secret, endpoint, action, options)
-
-
-class Command(object):
-    """
-    A representation of an EC2 API method call that can make a request and
-    display the response received from the backend cloud.
-    """
-
-    def __init__(self, key, secret, endpoint, action, parameters, output=None):
-        self.key = key
-        self.secret = secret
-        self.endpoint = endpoint
-        self.action = action
-        self.parameters = parameters
-        if output is None:
-            output = sys.stdout
-        self.output = output
-
-    def run(self):
-        pass
 
 
 def main(arguments):
