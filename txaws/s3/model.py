84568bbe82931594c010b82ea06af15a459c5414
Jamu Kakar <jkakar@kakar.ca>
2011-03-26 13:32:44 +0100

    - Minor cosmetic changes.

--- txaws/s3/model.py
+++ txaws/s3/model.py
@@ -1,5 +1,6 @@
 from txaws.util import XML
 
+
 class Bucket(object):
     """
     An Amazon S3 storage bucket.
@@ -52,24 +53,36 @@ class FileChunk(object):
     These chunks need to be assembled once they are all returned.
     """
 
-class RequestPayment(object):
 
-    payer_choices = ('Requester', 'BucketOwner')
+class RequestPayment(object):
+    """
+    A payment request.
+
+    @param payer: One of 'Requester' or 'BucketOwner'.
+    """
+
+    payer_choices = ("Requester", "BucketOwner")
 
     def __init__(self, payer):
         if payer not in self.payer_choices:
-            raise ValueError('Invalid value for payer: `%s`. Must be one of %s' %
-                             (payer, ','.join(self.payer_choices)))
+            raise ValueError("Invalid value for payer: `%s`. Must be one of "
+                             "%s." % (payer, ",".join(self.payer_choices)))
         self.payer = payer
 
     def to_xml(self):
-        return ('<RequestPaymentConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">\n'
-                '  <Payer>%s</Payer>\n'
-                '</RequestPaymentConfiguration>' % self.payer)
+        """
+        Convert this request into a C{RequestPaymentConfiguration} XML
+        document.
+        """
+        return ("<RequestPaymentConfiguration "
+                  'xmlns="http://s3.amazonaws.com/doc/2006-03-01/">\n'
+                "  <Payer>%s</Payer>\n"
+                "</RequestPaymentConfiguration>" % self.payer)
 
     @classmethod
     def from_xml(cls, xml_bytes):
+        """
+        Create an instance from a C{RequestPaymentConfiguration} XML document.
+        """
         root = XML(xml_bytes)
-        return cls(root.findtext('Payer'))
-
-
+        return cls(root.findtext("Payer"))
