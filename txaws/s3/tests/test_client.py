3b8bd7126d1f94ee2bf5f462f97ef3b2fa683814
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-01-02 11:12:08 -0500

    Use custom variable names to avoid accidentally using someone's legit credentials.

--- txaws/s3/tests/test_client.py
+++ txaws/s3/tests/test_client.py
@@ -1,6 +1,7 @@
 import datetime
 from hashlib import sha256
 import warnings
+from os import environ
 
 from twisted.internet.defer import succeed
 
@@ -1322,31 +1323,37 @@ class MiscellaneousTestCase(TXAWSTestCase):
 
 
 def get_live_client(case):
+    # Find credentials from the environment.
+    #
+    # To run this test, set TXAWS_INTEGRATION_AWS_ACCESS_KEY_ID
+    # and TXAWS_INTEGRATION_AWS_SECRET_ACCESS_KEY to some
+    # legitimate credentials.  It is probably a good idea to limit
+    # what these credentials are allowed to do:
+    #
+    #    - in case they leak out of the test suite somehow
+    #
+    #    - in case the implementation is broken and does something destructive
+    #
+    #    - in case malicious code is inserted somehow (eg, you run
+    #      tests on code submitted by another developer)
+    #
+    # As far as I can tell there's no way to isolate an API user
+    # from _some_ of the parent account's S3 buckets.  Therefore,
+    # isolation probably involves registering a new top-level AWS
+    # account and dedicating it to testing purposes.
     try:
-        # Find credentials from the environment.
-        #
-        # To run this test, set AWS_ACCESS_KEY_ID and
-        # AWS_SECRET_ACCESS_KEY to some legitimate credentials.  It is
-        # probably a good idea to limit what these credentials are
-        # allowed to do:
-        #
-        #    - in case they leak out of the test suite somehow
-        #
-        #    - in case the implementation is broken and does something destructive
-        #
-        #    - in case malicious code is inserted somehow (eg, you run
-        #      tests on code submitted by another developer)
-        #
-        # As far as I can tell there's no way to isolate an API user
-        # from _some_ of the parent account's S3 buckets.  Therefore,
-        # isolation probably involves registering a new top-level AWS
-        # account and dedicating it to testing purposes.
-        credentials = AWSCredentials()
-    except ValueError as e:
-        case.skipTest(str(e))
-    aws = AWSServiceRegion(credentials)
-    s3 = aws.get_s3_client()
-    return s3
+        access_key = environ["TXAWS_INTEGRATION_AWS_ACCESS_KEY_ID"]
+        secret_key = environ["TXAWS_INTEGRATION_AWS_SECRET_ACCESS_KEY"]
+    except KeyError as e:
+        case.skipTest("Missing {} environment variable.".format(e))
+    else:
+        credentials = AWSCredentials(
+            access_key=access_key,
+            secret_key=secret_key,
+        )
+        aws = AWSServiceRegion(credentials)
+        s3 = aws.get_s3_client()
+        return s3
 
 
 # XXX These leak resources onto AWS.
